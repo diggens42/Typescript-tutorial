30c1dd1aa33abfcc0a0c94b952d35a1b207ca09a
diggens42 <frederic.marcel.wahl@gmail.com>
2025-03-28 18:27:25 +0100

    operators

---
+++ code/src/operators.ts
@@ -0,0 +1,40 @@
+// arithmetic ops:
+// + addition
+// - substration
+// * multiplication
+// / division
+// % modulo
+// ++ increment
+// -- decrement
+
+
+//relational ops
+// > greater than
+// < lesser than
+// >= greater than or equal to
+// <= lesser than or equal to
+// == equality
+// != not equal
+
+//logical ops:
+// && AND
+// || OR
+// ! NOT
+
+//bitwise ops:
+// & Bitwise AND
+// | Bitwise OR
+// ^ Bitwise XOR It performs a Boolean exclusive OR operation on each bit of its integer arguments. Exclusive OR means that either operand one is true or operand two is true, but not both.
+// ~ Bitwise NOT reverses all bits
+// << Left Shift
+// >> Right Shift
+// >>> Right Shift with Zero --> This operator is just like the >> operator, except that the bits shifted in on the left are always zero.
+
+
+//assignemnt ops:
+// =  simple assignment
+// += add and assignment
+// -= subtract and assignment
+// *= multiply and assignment
+// /= divide and assignment
+
